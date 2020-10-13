56ac1177dd5ee7c52a77a07cdbef88a4f256e059
SimoneMat <simona.matiukaite@gmail.com>
2020-10-13 18:17:58 -0400

    Add files via upload

---
+++ 5.10.cpp
@@ -0,0 +1,14 @@
+/*
+Task: write a for loop that displays all of the odd numbers, 1 throught 49
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	for (int num = 1; num < 50; num +=2)
+	cout << num << endl;
+
+	return 0;
+}
